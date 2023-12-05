dc355d2ddf564dd45c668043b4dcf277d64bba97
sjmitch-git <sjmitch20@outlook.com>
2023-12-05 18:43:56 +0000

    Implements useEffect hook

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,34 @@
-// make an api call using useEffect
+import { useEffect, useState } from "react";
+
+const endPoint = "https://jsonplaceholder.typicode.com/todos/1";
 
 export const APICall = () => {
+  const [todo, setTodo] = useState();
 
-    // Your code here!
+  const fetchTodo = async () => {
+    try {
+      const response = await fetch(endPoint);
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setTodo(data.title); // Assuming the response has a 'title' property
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodo();
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>{todo}</p>
+    </>
+  );
+};
